43428c17f5f0624e67f1b46e293c32a86cac3c82
ttrdev <arturik@MacBook-Pro-Artur.local>
2019-03-22 21:37:12 +0500

    rename host

--- server.py
+++ server.py
@@ -114,7 +114,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://dota2ls.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://dotalivebot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
